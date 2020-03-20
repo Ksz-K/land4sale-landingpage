915e016d1b6e009659a0dca15a8bfa7ff8ed3b0d
Krzysztof Szachna-Krause <kszk@gmx.us>
2020-03-20 11:02:37 +0100

    google maps added

--- script.js
+++ script.js
@@ -1,34 +1,99 @@
 document.getElementById("searchClock").disabled = true;
 
-function zegar() {
-    var data = new Date();
-    var godzina = data.getHours();
-    var min = data.getMinutes();
-    var sek = data.getSeconds();
-    var terazjest = "" + godzina +
-        ((min < 10) ? ":0" : ":") + min +
-        ((sek < 10) ? ":0" : ":") + sek;
-    document.getElementById('searchClock').placeholder = terazjest;
+function clock() {
+  var data = new Date();
+  var godzina = data.getHours();
+  var min = data.getMinutes();
+  var sek = data.getSeconds();
+  var terazjest =
+    "" +
+    godzina +
+    (min < 10 ? ":0" : ":") +
+    min +
+    (sek < 10 ? ":0" : ":") +
+    sek;
+  document.getElementById("searchClock").placeholder = terazjest;
 }
-setInterval(zegar, 1000);
+setInterval(clock, 1000);
 
 function windowpop(url, width, height) {
-    var leftPosition, topPosition;
-    //Allow for borders.
-    leftPosition = (window.screen.width / 2) - ((width / 2) + 10);
-    //Allow for title and status bars.
-    topPosition = (window.screen.height / 2) - ((height / 2) + 50);
-    //Open the window.
-    window.open(url, "Window2", "status=no,height=" + height + ",width=" + width + ",resizable=yes,left=" + leftPosition + ",top=" + topPosition + ",screenX=" + leftPosition + ",screenY=" + topPosition + ",toolbar=no,menubar=no,scrollbars=no,location=no,directories=no");
+  var leftPosition, topPosition;
+  //Allow for borders.
+  leftPosition = window.screen.width / 2 - (width / 2 + 10);
+  //Allow for title and status bars.
+  topPosition = window.screen.height / 2 - (height / 2 + 50);
+  //Open the window.
+  window.open(
+    url,
+    "Window2",
+    "status=no,height=" +
+      height +
+      ",width=" +
+      width +
+      ",resizable=yes,left=" +
+      leftPosition +
+      ",top=" +
+      topPosition +
+      ",screenX=" +
+      leftPosition +
+      ",screenY=" +
+      topPosition +
+      ",toolbar=no,menubar=no,scrollbars=no,location=no,directories=no"
+  );
 }
 
-document.querySelector('form').addEventListener('submit', function (event) {
+function initMap() {
+  var options = {
+    zoom: 13.3,
+    center: { lat: 52.1577, lng: 21.07 }
+  };
+  var map = new google.maps.Map(document.getElementById("map"), options);
 
+  addMarker({
+    coords: { lat: 52.126999, lng: 21.139313 },
+    icon: "http://maps.google.com/mapfiles/kml/pushpin/grn-pushpin.png",
+    content:
+      '<div id="content">' +
+      '<div id="siteNotice">' +
+      "</div>" +
+      '<h1 id="firstHeading" class="firstHeading">Działka na sprzedaż</h1>' +
+      '<div id="bodyContent">' +
+      "<p><b>Obrys prostokąt:</b> ~33m x ~230m</p>" +
+      "<p><b>Powierzchnia:</b>7 597 m<sup>2</sup></p>" +
+      '<img src="view.png" alt="Działka Okrzeszyn" width="200" height="80" />' +
+      ' <p>Kontakt: <a href="https://okrzeszyn.waw.pl/#kontakt">' +
+      "Skorzystaj z formularza</a> " +
+      "</p>" +
+      "</div>" +
+      "</div>"
+  });
+  addMarker({
+    coords: { lat: 52.16326, lng: 21.139 },
+    icon: "http://maps.google.com/mapfiles/kml/shapes/highway.png",
+    content:
+      '<div id="content">' +
+      '<div id="siteNotice">' +
+      "</div>" +
+      '<h1 id="firstHeading" class="firstHeading">Most Południowy</h1>' +
+      '<div id="bodyContent">' +
+      "<p><b>Obwodnica Warszawy</b> - planowany termin oddania do ruchu <b>2020</b></p>" +
+      '<img src="bridgeSouth.png" alt="Most Południowy" width="200" height="80" />' +
+      "</div>" +
+      "</div>"
+  });
+  function addMarker(props) {
+    var marker = new google.maps.Marker({
+      position: props.coords,
+      map,
+      animation: google.maps.Animation.DROP,
+      icon: props.icon
+    });
+    var infoWindow = new google.maps.InfoWindow({
+      content: props.content
+    });
 
-    var emailAddressInput = event.target.querySelector('input[name="email"]');
-
-    if (emailAddressInput.value.indexOf('@') > 0) {
-        print('Dziękuję. Skontaktuje się wkrótce.');
-    }
-
-})
+    marker.addListener("click", function() {
+      infoWindow.open(map, marker);
+    });
+  }
+}
